b0224e076910ab24546ad54bd7a972c61a375870
knokko <knokogator@hotmail.com>
2020-12-08 19:25:09 +0100

    Start with the Mouse refactoring

--- src/events/mouse/mod.rs
+++ src/events/mouse/mod.rs
@@ -4,6 +4,40 @@ mod point;
 pub use button::*;
 pub use point::*;
 
+/// Represents a mouse, or something else that can generate events *at screen
+/// positions* (like clicking, moving, dragging...). 
+/// 
+/// On phones, mouses are usually fingers. On desktops, mouses are usually the
+/// mouses, but could also be controllers for instance.
+/// 
+/// ### Obtaining instances
+/// All mouse events have a `get_mouse()` method to get the `Mouse` that generated
+/// it. Alternatively, components can use the `get_local_mouses()` or the
+/// `get_all_mouses()` method of its buddy.
+/// 
+/// ### Creating instances
+/// The `new` function can be used to construct `Mouse`s, but only the 
+/// *provider* should do this.
+#[derive(Copy, Clone, Eq, PartialEq, Debug)]
+pub struct Mouse {
+    id: u16
+}
+
+impl Mouse {
+
+    /// Constructs a new `Mouse` with the given `id`. Only the *provider* should
+    /// use this function.
+    pub fn new(id: u16) -> Self {
+        Self { id }
+    }
+
+    /// Gets the numerical id of this `Mouse`. This method is mostly useful for the
+    /// *provider*, but components might also find this method useful.
+    pub fn get_id(&self) -> u16 {
+        self.id
+    }
+}
+
 /// The event is for the `on_mouse_click` method of `Component`.
 /// This event indicates that the user clicked *on* the component.
 /// 
@@ -11,16 +45,22 @@ pub use point::*;
 /// to keep track of mouse clicks outside the component.
 pub struct MouseClickEvent {
 
+    mouse: Mouse,
     point: MousePoint,
     button: MouseButton
 }
 
 impl MouseClickEvent {
 
-    /// Constructs a new `MouseClickEvent` with the given relative mouse
+    /// Constructs a new `MouseClickEvent` with the given mouse, relative mouse
     /// cursor position (point) and the given button
-    pub fn new(point: MousePoint, button: MouseButton) -> Self {
-        Self { point, button }
+    pub fn new(mouse: Mouse, point: MousePoint, button: MouseButton) -> Self {
+        Self { mouse, point, button }
+    }
+
+    /// Gets the `Mouse` that was clicked
+    pub fn get_mouse(&self) -> Mouse {
+        self.mouse
     }
 
     /// Gets the position of the mouse cursor, relative to the component that
@@ -29,7 +69,7 @@ impl MouseClickEvent {
         self.point
     }
 
-    /// Gets the mouse button that was clicked with
+    /// Gets the mouse button that was clicked
     pub fn get_button(&self) -> MouseButton {
         self.button
     }
@@ -46,39 +86,61 @@ impl MouseClickEvent {
 /// but only which mouse button was used.
 pub struct MouseClickOutEvent {
     
+    mouse: Mouse,
     button: MouseButton
 }
 
 impl MouseClickOutEvent {
 
-    pub fn new(button: MouseButton) -> Self { Self { button } }
+    /// Constructs a new `MouseClickOutEvent` with the given `Mouse` and
+    /// `MouseButton`
+    pub fn new(mouse: Mouse, button: MouseButton) -> Self { 
+        Self { mouse, button }
+    }
 
-    /// Gets the mouse button that was clicked with
+    /// Gets the `Mouse` that was clicked
+    pub fn get_mouse(&self) -> Mouse {
+        self.mouse
+    }
+
+    /// Gets the `MouseButton` that was clicked
     pub fn get_button(&self) -> MouseButton { self.button }
 }
 
-/// This method is for the on_mouse_move method of `Component`. It indicates
+/// This method is for the `on_mouse_move` method of `Component`. It indicates
 /// that the user moved the mouse *within* the component: both the position
 /// the mouse came from and the position the mouse went to are in the component.
 /// 
+/// ### Mouse leaving behavior
 /// If the user moved the mouse from a position *f* inside the component to a 
 /// position *t* outside the component, a `MouseMoveEvent` will be fired from *f*
 /// to the border *b* of the component where the mouse left the component. 
 /// Additionally, a `MouseLeaveEvent` with position *b* will be fired.
 /// 
+/// ### Mouse entering behavior
 /// If the user moved the mouse from a position *f* outside the component to a
 /// position *t* inside the component, a `MouseMoveEvent` will be fired from the
 /// border *b* of the component where the mouse came in to *t*. Additionally,
 /// a `MouseEnterEvent` with position *b* will be fired.
 pub struct MouseMoveEvent {
 
+    mouse: Mouse,
     from: MousePoint,
     to: MousePoint
 }
 
 impl MouseMoveEvent {
 
-    pub fn new(from: MousePoint, to: MousePoint) -> Self { Self { from, to } }
+    /// Constructs a new `MouseMoveEvent` indicating that `mouse` moved from
+    /// `from` to `to`
+    pub fn new(mouse: Mouse, from: MousePoint, to: MousePoint) -> Self { 
+        Self { mouse, from, to }
+    }
+
+    /// Gets the `Mouse` that was moved
+    pub fn get_mouse(&self) -> Mouse {
+        self.mouse
+    }
 
     /// Gets the position the mouse cursor came from (the old mouse position)
     pub fn get_from(&self) -> MousePoint { self.from }
@@ -87,42 +149,70 @@ impl MouseMoveEvent {
     pub fn get_to(&self) -> MousePoint { self.to }
 }
 
-/// The event for the on_mouse_enter method of `Component`. It indicates that the
+/// The event for the `on_mouse_enter` method of `Component`. It indicates that the
 /// user just moved the mouse inside the component.
 /// 
-/// This event captures the entrance_point, a position at the border of the 
-/// component at which the mouse entered the component. 
+/// This event captures the entrance_point, the position where the mouse 'set foot'
+/// inside the component. For regular mouses, this position will always be on one
+/// of the borders of the component. But for other 'mouses' (like fingers on phones),
+/// this can be anywhere in the component.
 pub struct MouseEnterEvent {
 
+    mouse: Mouse,
     entrance_point: MousePoint
 }
 
 impl MouseEnterEvent {
 
-    pub fn new(entrance_point: MousePoint) -> Self { Self { entrance_point } }
+    /// Constructs a new `MouseEnterEvent` with the given `Mouse` and `entrance_point`
+    pub fn new(mouse: Mouse, entrance_point: MousePoint) -> Self { 
+        Self { mouse, entrance_point } 
+    }
 
-    /// Gets the position where the mouse entered the component. This position
-    /// will always be at one of the borders of the component (or very close due
-    /// to rounding errors of floating point numbers).
+    /// Gets the `Mouse` that entered the component
+    pub fn get_mouse(&self) -> Mouse {
+        self.mouse
+    }
+
+    /// Gets the position where the mouse 'set foot' inside the component. 
+    /// 
+    /// For regular mouses, this will always be on the border of the component, but
+    /// this doesn't have to be the case for other `Mouse`s like fingers on
+    /// touchscreens.
     pub fn get_entrance_point(&self) -> MousePoint { self.entrance_point }
 }
 
-/// The event for the on_mouse_leave method of `Component`. This event indicates
+/// The event for the `on_mouse_leave` method of `Component`. This event indicates
 /// that the user just moved the mouse cursor outside the component. 
 /// 
-/// This event captures the exit_point, a position at the border of the component
-/// where the mouse left the component.
+/// This event captures the exit_point, the position where the mouse cursor
+/// 'Stepped out' of the component.
+/// 
+/// For regular mouses, this will always be on the border of the component, but
+/// this doesn't have to be the case for other `Mouse`s like fingers on
+/// touchscreens.
 pub struct MouseLeaveEvent {
 
+    mouse: Mouse,
     exit_point: MousePoint
 }
 
 impl MouseLeaveEvent {
 
-    pub fn new(exit_point: MousePoint) -> Self { Self { exit_point } }
+    /// Constructs a new `MouseLeaveEvent` with the given `exit_point`
+    pub fn new(mouse: Mouse, exit_point: MousePoint) -> Self { 
+        Self { mouse, exit_point } 
+    }
 
-    /// Gets the position where the mouse left the component. This position will
-    /// always be at the border of the component (or almost due to rounding errors
-    /// of floating point numbers).
+    /// Gets the `Mouse` that left the component
+    pub fn get_mouse(&self) -> Mouse {
+        self.mouse
+    }
+
+    /// Gets the position where the mouse left the component. 
+    /// 
+    /// For regular mouses, this will always be on the border of the component, but
+    /// this doesn't have to be the case for other `Mouse`s like fingers on
+    /// touchscreens.
     pub fn get_exit_point(&self) -> MousePoint { self.exit_point }
 }
