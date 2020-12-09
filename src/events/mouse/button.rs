dc830faeb8599a1df9c9a70d5b536053bb91ee8f
knokko <knokogator@hotmail.com>
2020-12-09 18:52:32 +0100

    Rework MouseButton

--- src/events/mouse/button.rs
+++ src/events/mouse/button.rs
@@ -1,13 +1,12 @@
-/// Represents one of the buttons of a mouse, for instance the left button
-/// or the right button. 
+/// Represents one of the buttons of a `Mouse`, for instance the primary button,
 /// 
 /// This struct is typically used for mouse events to indicate which button
 /// was clicked or pressed.
 /// 
-/// Most users would simply use the `is_left`, `is_right`
-/// and `is_wheel` methods to check which mouse button it was, but there is
-/// also a `get_index` method to get the numerical index of the button. That
-/// method can be used to track for instance macro mouse buttons.
+/// Every `Mouse` has a *primary* button and 0 or more additional buttons. The
+/// `is_primary` method can be used to check whether a button is the primary
+/// button. There is also the `get_index` method, which can be used to distinguish
+/// the other buttons.
 #[derive(Clone,Copy,PartialEq,Eq,Debug)]
 pub struct MouseButton {
 
@@ -16,97 +15,29 @@ pub struct MouseButton {
 
 impl MouseButton {
 
+    /// Constructs a new `MouseButton` with the given `index`. This function
+    /// should normally only be used by the *provider*.
     pub const fn new(index: u8) -> Self { Self { index } }
+
+    /// Constructs an instance of `MouseButton` that represents the *primary*
+    /// button of a `Mouse`.
+    pub const fn primary() -> Self {
+        Self { index: 0 }
+    }
 
     /// Gets the numerical index of this mouse button. 
     /// 
-    /// Normally, a value of 0 indicates a left click, a value of 1 indicates a 
-    /// mouse wheel click and a value of 2 indicates a right click. Other values
-    /// indicate that some other button was used, for instance a macro button. 
+    /// This will always be 0 for the primary button, and some other value for
+    /// the other buttons.
     /// 
-    /// If you are only interested in the common mouse buttons (left, wheel
-    /// and right), you can use the `is_left`, `is_right` and `is_wheel` methods
-    /// instead of this method.
+    /// This method is particularly useful to distinguish the other buttons from
+    /// each other. Use the `is_primary` button if you just want to check whether
+    /// this is the primary mouse button.
     pub fn get_index(&self) -> u8 { self.index }
 
-    /// Returns true if this button is the left mouse button
-    pub fn is_left(&self) -> bool { *self == LEFT_MOUSE_BUTTON }
-
-    /// Returns true if this button is the right mouse button
-    pub fn is_right(&self) -> bool { *self == RIGHT_MOUSE_BUTTON }
-
-    /// Returns true if this button is the mouse wheel (most mouse wheels can
-    /// not only be used for scrolling, but can also be pressed and released,
-    /// which allow it to be used as a button).
-    pub fn is_wheel(&self) -> bool { *self == MOUSE_WHEEL_BUTTON }
-}
-
-/// A constant representating the left mouse button. This constant should be
-/// convenient when calling methods that check if a certain mouse button is
-/// down. 
-pub const LEFT_MOUSE_BUTTON: MouseButton = MouseButton::new(0);
-
-/// A constant representating the right mouse button. This constant should be
-/// convenient when calling methods that check if a certain mouse button is
-/// down. 
-pub const RIGHT_MOUSE_BUTTON: MouseButton = MouseButton::new(2);
-
-/// A constant representating the 'mouse wheel button`. The mouse wheel is
-/// normally used to scroll, but most mouse wheels can also be pressed. 
-/// This constant should be convenient when calling methods that check if a 
-/// certain mouse button is down. 
-pub const MOUSE_WHEEL_BUTTON: MouseButton = MouseButton::new(1);
-
-#[cfg(test)]
-mod tests {
-
-    use super::*;
-
-    #[test]
-    fn test_left() {
-
-        // Positive tests
-        assert!(LEFT_MOUSE_BUTTON.is_left());
-        assert!(MouseButton::new(0).is_left());
-        assert!(MouseButton::new(0) == LEFT_MOUSE_BUTTON);
-
-        // Negative tests
-        assert!(!RIGHT_MOUSE_BUTTON.is_left());
-        assert!(!MouseButton::new(4).is_left());
-        assert!(MouseButton::new(4) != LEFT_MOUSE_BUTTON);
-    }
-
-    #[test]
-    fn test_right() {
-
-        // Positive tests
-        assert!(RIGHT_MOUSE_BUTTON.is_right());
-        assert!(MouseButton::new(2).is_right());
-        assert!(MouseButton::new(2) == RIGHT_MOUSE_BUTTON);
-
-        // Negative tests
-        assert!(!LEFT_MOUSE_BUTTON.is_right());
-        assert!(!MouseButton::new(4).is_right());
-        assert!(MouseButton::new(4) != RIGHT_MOUSE_BUTTON);
-    }
-
-    #[test]
-    fn test_wheel() {
-        assert!(MOUSE_WHEEL_BUTTON.is_wheel());
-        assert!(MouseButton::new(1).is_wheel());
-        assert!(MouseButton::new(1) == MOUSE_WHEEL_BUTTON);
-
-        // Negative tests
-        assert!(!LEFT_MOUSE_BUTTON.is_wheel());
-        assert!(!MouseButton::new(4).is_wheel());
-        assert!(MouseButton::new(4) != MOUSE_WHEEL_BUTTON);
-    }
-
-    #[test]
-    fn test_other() {
-
-        assert!(MouseButton::new(5).get_index() == 5);
-        assert!(MouseButton::new(6) == MouseButton::new(6));
-        assert!(MouseButton::new(5) != MouseButton::new(6));
+    /// Checks whether this mouse button is the primary buttons (and returns true if
+    /// and only if that is the case)
+    pub fn is_primary(&self) -> bool {
+        self.index == 0
     }
 }
