eee593014d91903ea6dac8ce4fc09c78e6a739e6
knokko <knokogator@hotmail.com>
2021-01-15 11:04:45 +0100

    Add an example for HoverColorCircleComponent

---
+++ examples/hover_color_circle.rs
@@ -0,0 +1,9 @@
+use knukki::*;
+
+fn main() {
+    let component = HoverColorCircleComponent::new(
+        Color::rgb(50, 50, 50),
+        Color::rgb(30, 200, 80)
+    );
+    start(Application::new(Box::new(component)), "Hover color circle");
+}
