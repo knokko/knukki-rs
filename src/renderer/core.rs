3d6d3b5da77d11b6450d9fd1a6bbfd1a8eb5e551
knokko <knokogator@hotmail.com>
2021-01-21 13:22:35 +0100

    Document the core methods of Renderer

--- src/renderer/core.rs
+++ src/renderer/core.rs
@@ -1,10 +1,17 @@
 use crate::*;
 
 impl Renderer {
+
+    /// Starts this `Renderer`. The `Application` is supposed to call this method each time before
+    /// it starts rendering its components.
+    ///
+    /// Currently, this method will only ensure that the viewport and scissor are up-to-date.
     pub fn start(&self) {
         self.apply_viewport_and_scissor();
     }
 
+    /// Sets the viewport and scissor of the rendering context (probably OpenGL) to the current
+    /// value of `self.get_viewport()` and `self.get_scissor()` respectively.
     #[cfg(not(feature = "golem_rendering"))]
     pub fn apply_viewport_and_scissor(&self) {
         // There is nothing to be done without a Golem context
@@ -12,18 +19,41 @@ impl Renderer {
 
     // TODO Clear color method
 
+    /// Gets the current viewport region of this `Renderer`. The drawing operations of components
+    /// will be scaled and translated to fit inside this region.
     pub fn get_viewport(&self) -> RenderRegion {
         let viewport_stack = self.viewport_stack.borrow();
         *viewport_stack.last().expect("Viewport stack is never empty")
     }
 
+    /// Gets the current scissor region of this `Renderer`. Components won't be able to draw
+    /// anything outside this region.
+    ///
+    /// When components draw something that is partially outside this region, the pixels outside
+    /// this region simply won't be affected, but the pixels inside this region will change
+    /// normally.
     pub fn get_scissor(&self) -> RenderRegion {
         let scissor_stack = self.scissor_stack.borrow();
         *scissor_stack.last().expect("Scissor stack is never empty")
     }
 
-    // TODO Document this
-    /// Note: to ensure glClear is consistent, this will also push a scissor
+    /// Shrinks the viewport (and scissor) by the given amounts, calls the `render_function`, and
+    /// thereafter restores the viewport and scissor.
+    ///
+    /// ## Edge case
+    /// But, if the shrunk viewport or scissor would have a width or height of 0, the
+    /// `render_function` will **not** be called, and this method will return `None`.
+    ///
+    /// ## Motivation
+    /// The motivation behind this function is to help menu components: they typically want to
+    /// render components inside an area that is smaller than the entire viewport. So, they call
+    /// this method, use the `(min_x, min_y, max_x, max_y)` parameters to define the region in
+    /// which the component is allowed to render, and call the `render` method of the component in
+    /// the `render_function`.
+    ///
+    /// ## Details
+    /// The `new_viewport` will be equal to `old_viewport.child_region(min_x, min_y, max_x, max_y)`
+    /// and the `new_scissor` will be equal to `old_scissor.intersection(new_viewport)`.
     pub fn push_viewport<R>(
         &self, min_x: f32, min_y: f32, max_x: f32, max_y: f32,
         render_function: impl FnOnce() -> R
@@ -46,10 +76,15 @@ impl Renderer {
                 viewport_stack.push(child_viewport);
                 drop(viewport_stack);
 
+                // Push the scissor
                 let mut scissor_stack = self.scissor_stack.borrow_mut();
                 scissor_stack.push(child_scissor);
                 drop(scissor_stack);
 
+                // Make sure the viewport and scissor are actually used
+                self.apply_viewport_and_scissor();
+
+                // Call the render function
                 let result = render_function();
 
                 // Pop the viewport and scissor
@@ -68,7 +103,12 @@ impl Renderer {
         }
     }
 
-    // TODO Document this
+    /// (Re-)sets the viewport and scissor of this `Renderer` to `new_viewport`. This will clear
+    /// the entire viewport stack and scissor stack.
+    ///
+    /// This method requires a mutable reference to `self` because it is intended to be used only
+    /// by the *provider*, which should call this before the `render` method of the `Application`,
+    /// to specify where the `Application` should be rendered.
     pub fn reset_viewport(&mut self, new_viewport: RenderRegion) {
         let mut viewport_stack = self.viewport_stack.borrow_mut();
         let mut scissor_stack = self.scissor_stack.borrow_mut();
