8496ea050a2f03abc3f7bd9108bbce3c86fa8394
knokko <knokogator@hotmail.com>
2021-04-29 12:00:19 +0200

    Use a more challenging test string in the text-display example

--- examples/text-display/menu.rs
+++ examples/text-display/menu.rs
@@ -6,11 +6,11 @@ pub fn create_app() -> Application {
     let mut menu = SimpleFlatMenu::new(Some(Color::rgb(100, 200, 50)));
 
     menu.add_component(Box::new(SimpleTextComponent::new(
-        "Hello, knukki!".to_string(),
+        "ᄍᄎᄏʡʢʣʤମଯରיך⽉⽊⽋כלםמןנឝឞសסעףפ綾菱ץ".to_string(),
         HorizontalTextAlignment::Center,
         VerticalTextAlignment::Center,
         None
-    )), ComponentDomain::between(0.0, 0.2, 1.0, 0.6));
+    )), ComponentDomain::between(0.0, 0.0, 1.0, 1.0));
 
     Application::new(Box::new(menu))
 }
